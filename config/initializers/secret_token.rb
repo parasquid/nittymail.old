1a195393a1acbcd5adb559f975862955528e3f56
tristan <tristan.gomez@gmail.com>
2012-02-08 01:53:57 +0800

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Nittymail::Application.config.secret_token = '02df9972443cdb100501f727842746471a94f7838d223ddc08d0d7ece4999b57f08f181d33af9ee14bad220d6550983fb3498aeb965f4cc4475239678deb2710'
