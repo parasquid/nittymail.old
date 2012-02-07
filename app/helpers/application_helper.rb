57c44e459798bf8511536accd576aad3ac32c5ba
tristan <tristan.gomez@gmail.com>
2012-02-08 06:30:41 +0800

    page specific css (plus foundation for having user-specific css)

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,17 @@
 module ApplicationHelper
+  
+  def find_named_routes
+    routes = []
+   
+    Rails.application.routes.named_routes.each do |name, route|
+      req = route.requirements
+      if req[:controller] == params[:controller] && req[:action] == params[:action]
+        routes << name
+      end
+    end
+
+    routes << params[:id] if params.include?(:id)
+    routes
+  end
+
 end
