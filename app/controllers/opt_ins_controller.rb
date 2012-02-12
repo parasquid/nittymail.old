05165546aa5cb3fcc8c55070b79c06a1d7d9f0d2
tristan <tristan.gomez@gmail.com>
2012-02-12 21:59:38 +0800

    done with opt-ins, fix missing template

--- app/controllers/opt_ins_controller.rb
+++ app/controllers/opt_ins_controller.rb
@@ -1,7 +1,6 @@
 class OptInsController < ApplicationController
   def confirm
     opt_in = OptIn.where(token: params[:opt_in_token], used: false).first || not_found
-    raise opt_in.inspect
     @subscriber = Subscriber.where(email: opt_in.subscriber).first
     @subscription = User.find_by_slug(opt_in.subscription)
     # add the subscriber to the mailing list
